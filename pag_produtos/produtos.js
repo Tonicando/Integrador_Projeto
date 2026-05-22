3daecde16c479451e77a095c4f71169c9fece441
Tonicando <937117minas@gmail.com>
2026-05-22 08:40:58 -0300

    Add files via upload

---
+++ pag_produtos/produtos.js
@@ -0,0 +1,25 @@
+
+document.addEventListener("DOMContentLoaded", function() { 
+    const botao = document.querySelectorAll(".categoria");
+    const produtos = document.querySelectorAll(".produto");
+
+    botao.forEach(botao => {
+        botao.addEventListener('click', () => {
+            const ativado = document.querySelector('.categoria.ativo');
+            if(ativado){
+                ativado.classList.remove('ativo');
+            }
+            botao.classList.add('ativo');
+
+            const peneira =  botao.getAttribute('data-filtro');
+            produtos.forEach(produto =>{
+                const categoria = produto.getAttribute('data-categoria');
+                if (peneira =='Todos' || peneira == categoria) {
+                    produto.classList.remove('transparente');}
+                else{
+                    produto.classList.add('transparente');
+                }
+            });
+        });
+    });
+});
